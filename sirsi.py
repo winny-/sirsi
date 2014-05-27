5246a39fb748b87e9296a4c1e38ff53bb48782ba
Winston Weinert <winston@ml1.net>
2014-05-26 23:13:39 -0500

    Clearer __repr__ for sirsi.Account

--- sirsi.py
+++ sirsi.py
@@ -149,6 +149,9 @@ class Account(object):
         return BeautifulSoup(response.read())
 
     def __repr__(self):
-        return '<{} {} {}>'.format(self.__class__.__name__,
-                                   self.catalog,
-                                   self.userid)
+        return '<{}.{} {} {}>'.format(
+            __name__,
+            self.__class__.__name__,
+            self.catalog,
+            self.userid,
+        )
