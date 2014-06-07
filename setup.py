2e91c826a72e3f240f6d010678d68bab0bab5749
Winston Weinert <winston@ml1.net>
2014-06-07 15:35:37 -0500

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from sirsi import __version__, __author__
+
+setup(
+    name='sirsi',
+    version=__version__,
+    author=__author__,
+    author_email='winston@ml1.net',
+    description='Manage a sirsi enterprise-based library account',
+    url='https://github.com/-winny/sirsi',
+    license='MIT',
+    packages=['sirsi'],
+    install_requires=[
+        'argparse==1.2.1',
+        'beautifulsoup4==4.3.2',
+        'mechanize==0.2.5',
+        'python-dateutil==2.2',
+        'tabulate==0.7.2',
+    ],
+)
