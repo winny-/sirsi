786da7ac63cb180b64246fa7204d9b0d2d743d1b
Winston Weinert <winston@ml1.net>
2015-01-13 09:14:24 -0600

    Add PyCharm glue script
    Can't find a way to DEBUG a Python module directly.

---
+++ run_sirsi.py
@@ -0,0 +1,10 @@
+"""
+PyCharm can't run packages. WTF?!
+
+Anyway, here is glue because PyCharm can't do a basic Pythonic task.
+"""
+
+__author__ = 'Winston Weinert'
+
+
+from sirsi import __main__
